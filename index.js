495f268b85c9dcdaedfc29efe583724d470433a2
llhe <helinglin@julanling.com>
2020-10-12 20:47:03 +0800

    git 操作自动化 init

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const { version } = require('./package.json')
 const commander = require('commander')
+const { execSync } = require('child_process')
+// const DEVELOP = execSync('git symbolic-ref --short -q HEAD', { encoding: 'utf8' }) // 开发分支名称
 
 // commander: start, test, pre, del
 commander.version(version, '-v --version', '获取版本').option('-s, --start').parse(process.argv)
 
+main()
 
-console.log(commander.start)
+function main() {
+  // 开始流程： dev test,
+  if (commander.start) {
+    start()
+  } else if (commander.test) {
+    // 提测流程
+  } else if (commander.pre) {
+    // 预发布流程
+  } else if (commander.del) {
+    // 删除dev test 分支
+  }
+}
+
+function start() {
+  try {
+    execSync('git checkout master')
+    execSync('git pull', { encoding: 'utf8' })
+  } catch (error) {
+    console.log(error)
+  }
+}
