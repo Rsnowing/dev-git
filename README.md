666a27a12748f2c6bc747fc4f84e671be9b35af5
llhe <helinglin@julanling.com>
2020-10-15 15:16:28 +0800

    功能完成

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 2. 签出pre => merge => push 
 3. 签出dev test pre
 4. 删除 dev test
+
+* g --help 查看帮助 
+* g -v or g --version 获取版本
+* g -s or g --start 开始开发 删除已有dev test pre 并从master重新签出这些分支
+* g -t or g --test 提测 将开发分支合到dev test 并push
+* g -p or g --pre 预发 将开发分支合到pre 并push。 如果没有pre分支自己建哦
+* g -d or g --del 上线完成 删除dev test。 注： pre你自己删吧 啦啦啦
--- index.js
+++ index.js
@@ -1,33 +1,72 @@
 const { version } = require('./package.json')
 const commander = require('commander')
 const { execSync } = require('child_process')
-// const DEVELOP = execSync('git symbolic-ref --short -q HEAD', { encoding: 'utf8' }) // 开发分支名称
+const DEVELOP = execSync('git symbolic-ref --short -q HEAD', { encoding: 'utf8' }) // 开发分支名称
 
 // commander: start, test, pre, del
-commander.version(version, '-v --version', '获取版本').option('-s, --start').parse(process.argv)
+commander
+  .version(version, '-v --version', '获取版本')
+  .option('-s, --start', '开始开发 删除已有dev test pre 并从master重新签出这些分支')
+  .option('-t, --test', '提测 将开发分支合到dev test 并push')
+  .option('-p, --pre', '预发 将开发分支合到pre 并push。 如果没有pre分支自己建哦')
+  .option('-d, --del', '上线完成 删除dev test。 注： pre你自己删吧 啦啦啦')
+  .parse(process.argv)
+
+commander.on('--help', () => {})
 
 main()
 
 function main() {
   // 开始流程： dev test,
   if (commander.start) {
-    start()
+    console.log('创建dev test pre分支')
+    del('dev')
+    del('test')
+    create('dev')
+    create('test')
+    create('pre')
   } else if (commander.test) {
+    console.log('准备提测')
     // 提测流程
+    merge('dev')
+    merge('test')
   } else if (commander.pre) {
+    console.log('准备预发, 如果没有pre分支自己建哦')
     // 预发布流程
+    merge('pre')
   } else if (commander.del) {
     // 删除dev test 分支
+    console.log('删除dev test, 注： pre你自己删吧 啦啦啦')
+    del('dev')
+    del('test')
   }
 }
 
-function start() {
+// 从master创建分支
+function create(branch) {
   try {
-    // execSync('git checkout master')
-    // execSync('git pull', { encoding: 'utf8' })
-    execSync('git checkout -b dev origin/master', { encoding: 'utf8' })
-    execSync('git push origin dev --set-upstream', { encoding: 'utf8' })
+    execSync(`git branch ${branch} origin/master`, { encoding: 'utf8' })
   } catch (error) {
-    console.log(error)
+    // console.log(error)
+  }
+}
+
+function merge(branch) {
+  try {
+    execSync(`git checkout ${branch}`)
+    execSync(`git merge --no-ff ${DEVELOP}`, { encoding: 'utf8' })
+    execSync(`git push origin ${branch}`, { encoding: 'utf8' })
+  } catch (error) {
+    // console.log(error)
+  }
+}
+
+// 删除本地及远程分支
+function del(branch) {
+  try {
+    execSync(`git branch --delete --force ${branch}`, { encoding: 'utf8' })
+    execSync(`git push origin --delete ${branch}`, { encoding: 'utf8' })
+  } catch (e) {
+    // console.error(e)
   }
 }
