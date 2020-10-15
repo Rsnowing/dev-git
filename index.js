d5cca305c5ed84a5e9ab28f8988eeec4313bbafe
llhe <helinglin@julanling.com>
2020-10-15 18:47:04 +0800

    pull

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ function create(branch) {
 function merge(branch) {
   try {
     execSync(`git checkout ${branch}`)
+    execSync(`git pull origin ${branch}`)
     execSync(`git merge --no-ff ${DEVELOP}`, { encoding: 'utf8' })
     execSync(`git push origin ${branch}`, { encoding: 'utf8' })
   } catch (error) {
